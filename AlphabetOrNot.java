d366d786f0413235599c3b51cfb8a54914239557
DharshinipriyaThiyagarajan <dharshinipriya.tcse@ksrce.ac.in>
2016-07-20 17:42:44 +0530

    Create AlphabetOrNot.java

---
+++ AlphabetOrNot.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AlphabetOrNot {
+	public static void main(String[] args) {
+		char c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Character:");
+		c=s.next().charAt(0);
+		int a=(char)c;
+		if(a>64&&a<91||a>96&&a<123){
+			System.out.println("The given character "+c+" is an alphabet character");
+				}
+		else
+		{
+			System.out.println("The given character "+c+" is not an alphabet character");
+		}
+		s.close();
+	}
+}
